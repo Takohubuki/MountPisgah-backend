ec7ec3b351f13e67c565bfe03cdc6c719aebd584
Tako <932421804@qq.com>
2024-06-11 20:26:01 +0800

    update

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+const s3 = new S3Client({ 
+    endpoint: 'sgp1.vultrobjects.com',
+    credentials: {
+        accessKeyId: 'accessKey',
+        secretAccessKey: 'secretKey'
+    }
+ });
 
 app.use(express.static('public'));
+
+console.log("直到我在毗斯迦山，发现我家极目一看，脱下帐篷，安静等候，信心变见，盼望成就")
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
